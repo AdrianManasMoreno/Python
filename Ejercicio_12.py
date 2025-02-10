c8068ef6b176de021ea2cbf2d5c9beff3fd39c3a
Adrián <adri.tauste.94@gmail.com>
2025-02-10 21:20:29 +0100

    Delete Ejercicio_12.py

--- Ejercicio_12.py
+++
@@ -1,6 +0,0 @@
-#Ejercicio 12. Función numeros_suma usando lambdas y map
-#lambda que sume 3 a cada número de una lista dada.
-
-lista_numeros = [24, 56, 2.3, 19, -1, 0]
-numeros_suma = lambda lista: list(map(lambda x : x + 3, lista))
-print(numeros_suma(lista_numeros))
